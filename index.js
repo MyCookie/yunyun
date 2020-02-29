bef3078cbb85094bb90166844380cf05ae975955
Ashish Kansara <ashishnkansara@gmail.com>
2020-02-28 23:33:23 -0700

    Yunyun no longer talks with herself. She also gives hugs and responds to gratitude

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   client.user
-    .setActivity("to her friends!", { type: "LISTENING" })
+    .setActivity(" her friends!", { type: "LISTENING" })
     .then(presence =>
       console.log(`Yunyun's ${presence.game ? presence.game.name : `none`}`)
     )
@@ -16,7 +16,10 @@ client.on("ready", () => {
 
 // yunyun helps her friends!
 client.on("message", msg => {
-  if (msg.content.startsWith("yunyun") || msg.content.startsWith("Yunyun")) {
+  if (
+    (msg.author.id != client.id) &&
+    msg.content.toLowerCase().startsWith("yunyun")
+  ) {
     if (msg.content.includes("ping")) {
       const replies = ["私の 名前は ゆんゆん です！", "このすば!", "Pong!"];
 
@@ -86,19 +89,37 @@ client.on("message", msg => {
     } else if (msg.content.includes("bless") && msg.mentions.users) {
       // change roles
       const user = msg.mentions.users.first();
-      if (user) {
-        const member = msg.guild.member(user);
-        if (member) {
-          const modRole = member.guild.roles.find(role => role.name === "Mod");
-          member
-            .addRole(modRole)
-            .then(`Added role ${modRole.name} to ${user.tag}`);
-          msg.channel
-            .send(`Your friends really trust you, ${member.nickname}!`)
-            .then(`Yunyun helped ${user.tag} help their friends!`)
-            .catch(console.error);
-        }
+      const member = msg.guild.member(user);
+      const author = msg.guild.member(msg.author);
+      const modRole = author.guild.roles.find(role => role.name === "Mod");
+
+      if (author.roles.has(modRole) && member) {
+        const modRole = member.guild.roles.find(role => role.name === "Mod");
+        member
+          .addRole(modRole)
+          .then(`Added role ${modRole.name} to ${user.tag}`);
+        msg.channel
+          .send(`Your friends really trust you, ${member.nickname}!`)
+          .then(`Yunyun helped ${user.tag} help their friends!`)
+          .catch(console.error);
+      } else {
+        msg.channel
+          .send(`Yunyun promises to help you get closer to your friends, ${msg.author.tag}!`)
+          .then(`A non-${modRole.name} user, ${msg.author.id}, tried to add ${user.id} to the role ${modRole.name}`)
+          .catch(console.error);
       }
+    } else if (msg.content.toLowerCase().includes("hug")) {
+      msg.channel
+        .send(
+          `Yunyun hug! https://media.giphy.com/media/XpgOZHuDfIkoM/giphy.gif`
+        )
+        .then(console.log(`Yunyun hugged ${msg.author.id}!`))
+        .catch(console.error);
+    } else if (msg.content.toLowerCase().includes("thank you")) {
+      msg.channel
+        .send(":smiling_face_with_3_hearts:")
+        .then(console.log(`Yunyun is greatful ${msg.author.id} is her friend!`))
+        .catch(console.error);
     } else if (msg.content.includes("inspect") && msg.mentions.users) {
       // console.log(msg.mentions.users);
       msg.mentions.users.forEach((value, key) => {
@@ -114,12 +135,15 @@ client.on("message", msg => {
           .catch(console.error);
       });
     } else {
-      if (msg.member.id != client.user.id)
-        msg.channel
-          .send("Yunyun is here!")
-          .then(`Yunyun was summoned by ${msg.author.tag}!`)
-          .catch(console.error);
+      msg.channel
+        .send("Yunyun is here!")
+        .then(`Yunyun was summoned by ${msg.author.tag}!`)
+        .catch(console.error);
     }
+  }
+
+  if (msg.content.toLowerCase().includes("why yunyun why")) {
+    msg.channel.send("Yunyun is only trying to help!");
   }
 });
 
